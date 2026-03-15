166bd5e21a4e87cc443f10219fc95a6cb230e721
Toni Mercadante <mercadanteneto@gmail.com>
2026-03-15 05:04:08 -0300

    using wgpu to render a blue window screen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use glyphon::cosmic_text::skrifa::instance;
 use std::sync::Arc;
+use wgpu::DeviceDescriptor;
 use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
@@ -15,18 +17,80 @@ pub struct State {
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-    is_surface_configured: bool,
     window: Arc<Window>,
 }
 
 impl State {
     pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
-        Ok(Self { window })
+        let instance = wgpu::Instance::default();
+        let surface = instance.create_surface(window.clone())?;
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                compatible_surface: Some(&surface),
+                ..Default::default()
+            })
+            .await?;
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor::default())
+            .await?;
+
+        let size = window.inner_size();
+        let config = surface
+            .get_default_config(&adapter, size.width, size.height)
+            .ok_or(anyhow::anyhow!("no surface config"))?;
+        surface.configure(&device, &config);
+
+        Ok(Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+        })
     }
 
-    pub fn resize(&mut self, _width: u32, _height: u32) {}
+    pub fn resize(&mut self, width: u32, height: u32) {
+        if width > 0 && height > 0 {
+            self.config.width = width;
+            self.config.height = height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
 
     pub fn render(&mut self) {
+        let frame = self.surface.get_current_texture().unwrap();
+        let view = frame
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
+
+        {
+            let _pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    depth_slice: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.05,
+                            g: 0.05,
+                            b: 1.00,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                ..Default::default()
+            });
+        } // _pass is dropped here, render pass ends
+
+        self.queue.submit([encoder.finish()]);
+        frame.present();
         self.window.request_redraw();
     }
 }
@@ -39,6 +103,7 @@ impl ApplicationHandler for App {
                 .unwrap(),
         );
         self.state = Some(pollster::block_on(State::new(window)).unwrap());
+        self.state.as_ref().unwrap().window.request_redraw();
     }
 
     fn window_event(
@@ -56,6 +121,11 @@ impl ApplicationHandler for App {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::Resized(size) => state.resize(size.width, size.height),
             WindowEvent::RedrawRequested => state.render(),
+            WindowEvent::KeyboardInput {
+                device_id,
+                event,
+                is_synthetic,
+            } => println!("{event:?}"),
             _ => {}
         }
     }
